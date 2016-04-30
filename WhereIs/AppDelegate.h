25e10c2e959c2cead0b0ff37b2d55343edf979e0
ShipuW <shipu.wsp@gmail.com>
2016-04-30 22:58:12 +0800

    first commit

---
+++ WhereIs/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhereIs
+//
+//  Created by admin on 16/4/30.
+//  Copyright © 2016年 ShipuW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
